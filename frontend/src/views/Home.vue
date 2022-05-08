3a5365c3f8323e1ae90e082fdefc5de24257b998
Olga Samara <olcha.olcha@gmail.com>
2022-05-08 17:42:57 +0100

    Cleaned default settings

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page">
+        <h1>Home</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "HomePage",
+    data() {
+        return {
+            msg: "Welcome to our Pong App",
+        };
+    },
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
