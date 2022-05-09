caef94ac54e6514a8f0064989cf2279569a7a12b
Aileen Chen <xchen@student.codam.nl>
2022-05-09 22:06:13 +0200

    fix callback error + implement session

--- backend/src/auth/oauth/oauth.strategy.ts
+++ backend/src/auth/oauth/oauth.strategy.ts
@@ -1,43 +1,33 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { ConfigService } from "@nestjs/config";
 import { Injectable } from "@nestjs/common";
-import { UserService } from "../../user/user.service";
 import { Strategy } from "passport-oauth2"
-import { Profile } from "passport";
+import AuthService from "../auth.service";
+import { HttpService } from "@nestjs/axios";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class OAuthStrategy extends PassportStrategy(Strategy, 'oauth') {
 	constructor(
 		private readonly config: ConfigService,
-		private readonly userService: UserService,
-	) {
+		private readonly authService: AuthService,
+		private readonly httpService: HttpService,
+		private readonly jwtService: JwtService,
+		) {
 		super ({
 			authorizationURL: config.get('INTRA_AUTH_URL'),
 			tokenURL: config.get('INTRA_TOKEN_URL'),
 			clientID: config.get('INTRA_CLIENT_ID'),
-			clientSecrete: config.get('INTRA_CLIENT_SECRET'),
+			clientSecret: config.get('INTRA_CLIENT_SECRET'),
 			callbackURL: config.get('INTRA_CALLBACK_URL'),
 		});
 	}
 
-	async validate(accessToken: string, refreshToken: string, profile: Profile, done: any) {
-		console.log("validate function");
-		console.log( profile );
-		return accessToken;
+	async validate(accessToken: string, refreshToken: string, profile: any, done: any) {
+		const data = await this.httpService.get('https://api.intra.42.fr/v2/me', {
+			headers: { Authorization: `Bearer ${ accessToken }` }, }).toPromise();
+		console.log(data.data.id);
+		// const jwt = await this.jwtService.signAsync({ id: data.data.id });
+		// return jwt;
 	}
-
-	// validate(payload: unknown): unknown {
-	// 	console.log(payload);
-		
-	// 	return payload;
-	// }
-
-	// async validate(
-	// 	payload: {
-	// 		sub: number;
-	// 		email: string;
-	// 	}) {
-	// 	const user = await this.userService.getByID(payload.sub);
-	// 	return user;
-	// }
 }
