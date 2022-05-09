2bf26f17214fa42f307c95ddb13fa608c957accc
Olga Samara <olcha.olcha@gmail.com>
2022-05-09 13:59:03 +0200

    added PrimeVue UI library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.ts";
+import PrimeVue from "primevue/config";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue);
+app.mount("#app");
