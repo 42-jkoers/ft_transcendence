ac246131cf13b1a29ada31e6de521e0b0984b0d7
ergunirem <iremthedeveloper@gmail.com>
2022-05-10 13:52:36 +0200

    moved socket creation to Chat.vue from services/socketioservice.js

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="page">
-        <h1>Chat</h1>
+<div class="container">
+    <form @submit.prevent="onSubmit" class="form">
+    <textarea v-model="input" placeholder="Type your message..." class="input" />
+    <div class="input-group-append">
+         <button class="send-button">Send</button>
     </div>
+    </form>
+  </div>
 </template>
+
 <script>
+import { io } from 'socket.io-client';
+
 export default {
     name: "ChatPage",
     data() {
@@ -11,6 +19,21 @@ export default {
             msg: "Welcome to our Chat",
         };
     },
+
+    mounted() {
+        this.socket = io('http://localhost:3000');
+    },
+
+    beforeUnmount() {
+        if (this.socket) {
+            this.socket.disconnect();
+        }
+    }
+
+       //register a catch-all listener -> useful during development:
+    // this.socket.onAny((event, ...args) => {
+    //   console.log(event, args);
+    // })
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
