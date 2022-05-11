2b25f31ea5caf217f135ca5494dc50856d23d138
Olga Samara <olcha.olcha@gmail.com>
2022-05-11 18:02:57 +0200

    Room relationship description added to User

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -1,20 +1,22 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
- 
+import { Room } from 'src/chat/room/entities/room.entity';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
 @Entity()
 export class User {
 	@PrimaryGeneratedColumn()
 	public id?: number;
-	
-	@Column({ name: "intra_ID", unique: true })
+
+	@Column({ name: 'intra_ID', unique: true })
 	public intraID: string;
-	
+
 	@Column({ unique: true })
 	public username: string;
 
 	@Column({ nullable: true })
 	public avatar: string;
-	
 
+	@ManyToMany(() => Room, (room) => room.users)
+	rooms: Room[];
 }
- 
+
 export default User;
