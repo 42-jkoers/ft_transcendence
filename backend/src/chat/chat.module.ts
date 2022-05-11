2ca13b9013f49ab5a6e77ae59c1cababbace9757
Olga Samara <olcha.olcha@gmail.com>
2022-05-11 17:10:53 +0200

    initial room resource created

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './gateway/chat.gateway';
 import { ChannelModule } from './channel/channel.module';
 import { MessageModule } from './message/message.module';
+import { RoomModule } from './room/room.module';
 
 @Module({
-  providers: [ChatGateway],
-  imports: [ChannelModule, MessageModule]
+	providers: [ChatGateway],
+	imports: [ChannelModule, MessageModule, RoomModule],
 })
 export class ChatModule {}
