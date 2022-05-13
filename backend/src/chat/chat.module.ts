f56d0bbedfaf0e7d87135540d5ebd2074708dc1a
Olga Samara <olcha.olcha@gmail.com>
2022-05-13 17:57:02 +0200

    Room created and current user added to the list

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -3,9 +3,17 @@ import { ChatGateway } from './gateway/chat.gateway';
 import { ChannelModule } from './channel/channel.module';
 import { MessageModule } from './message/message.module';
 import { RoomModule } from './room/room.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomEntity } from './room/entities/room.entity';
+import { RoomService } from './room/room.service';
 
 @Module({
-	providers: [ChatGateway],
-	imports: [ChannelModule, MessageModule, RoomModule],
+	providers: [ChatGateway, RoomService],
+	imports: [
+		ChannelModule,
+		MessageModule,
+		RoomModule,
+		TypeOrmModule.forFeature([RoomEntity]),
+	],
 })
 export class ChatModule {}
--- backend/src/chat/room/room.service.ts
+++ backend/src/chat/room/room.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@nestjs/common';
-import { CreateRoomDto } from './dto/create-room.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { UpdateRoomDto } from './dto/update-room.dto';
+import { RoomEntity } from './entities/room.entity';
+import { IRoom } from './room.interface';
+import { IUser } from 'src/user/user.interface';
+import {
+	IPaginationOptions,
+	paginate,
+	Pagination,
+} from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class RoomService {
-	create(createRoomDto: CreateRoomDto) {
-		return 'This action adds a new room';
+	constructor(
+		@InjectRepository(RoomEntity)
+		private readonly roomRepository: Repository<RoomEntity>,
+	) {}
+	async createRoom(room: IRoom, creator: IUser): Promise<IRoom> {
+		const newRoom = await this.addCreatorToRoom(room, creator); // adding current creator to the array of users for this new room
+		return this.roomRepository.save(newRoom); // Saves a given entity in the database. If entity does not exist in the database then inserts, otherwise updates.
+	}
+
+	async addCreatorToRoom(room: IRoom, creator: IUser): Promise<IRoom> {
+		room.users.push(creator);
+		return room;
+	}
+
+	async getRoomsForUser(
+		userId: number,
+		options: IPaginationOptions,
+	): Promise<Pagination<IRoom>> {
+		//build SQL query to get rooms
+		// leftJoin will be referencing the property users defined in the RoomEntity.
+		const query = this.roomRepository
+			.createQueryBuilder('room')
+			.leftJoin('room.users', 'user')
+			.where('user.id = :userId', { userId });
+
+		return paginate(query, options);
 	}
 
 	findAll() {
