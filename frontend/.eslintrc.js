31e1ac9af761e769b3e42bc1c368380c3af166f0
Olga Samara <olcha.olcha@gmail.com>
2022-05-16 12:05:17 +0200

    html-indent and html-self-closing rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     rules: {
         // override/add rules settings here, such as:
         // 'vue/no-unused-vars': 'error'
+        "vue/html-indent": "off",
+        "vue/html-self-closing": "off",
     },
     env: {
         node: true,
