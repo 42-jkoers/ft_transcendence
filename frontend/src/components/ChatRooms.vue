b2da1b1c31c9b183578677a7831468f8c968247b
Olga Samara <olcha.olcha@gmail.com>
2022-05-16 12:10:57 +0200

    ChatRooms vue component added

---
+++ frontend/src/components/ChatRooms.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="chatrooms">
+        <PrimeVueButton
+            label="New Chat Room"
+            icon="pi pi-plus"
+            class="p-button-info"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import PrimeVueButton from "primevue/button";
+
+export default {
+    name: "ChatRooms",
+    components: {
+        PrimeVueButton,
+    },
+};
+</script>
