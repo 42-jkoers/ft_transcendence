6deba17ad0c380b5f4efda8ccc0ed8891ff42a93
Olga Samara <olcha.olcha@gmail.com>
2022-05-16 12:20:14 +0200

    body styling added(bg is commented for a while)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">
-      Home
-    </router-link> |
-    <router-link to="/chat">
-      Chat
-    </router-link>
-    <router-view />
-  </div>
+    <div id="nav">
+        <router-link to="/"> Home </router-link> |
+        <router-link to="/chat"> Chat </router-link>
+        <router-view />
+    </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'App',
-  components: {
-  },
+    name: "App",
+    components: {},
 };
 </script>
 <style>
@@ -24,6 +19,17 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    /* color: #ffffff; */
     margin-top: 60px;
+}
+body {
+    margin: 0;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+    /* background-color: var(--surface-a);
+    font-family: var(--font-family);
+    font-weight: 400;
+    color: var(--text-color); */
 }
 </style>
