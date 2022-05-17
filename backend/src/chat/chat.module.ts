27ac26a6d56707a6cd8bbaaa236de488cc8deeef
Irem Ergun <cikrikciogluirem@gmail.com>
2022-05-17 12:22:02 +0200

    ConnectedUser changed to ConnectedUserEntity

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -4,9 +4,11 @@ import { ChannelModule } from './channel/channel.module';
 import { MessageModule } from './message/message.module';
 import { AuthModule } from '../auth/auth.module';
 import { ConnectedUserService } from './connected-user/connected-user.service';
+import { ConnectedUserEntity } from 'src/chat/connected-user/connected-user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [ChatGateway, ConnectedUserService],
-  imports: [ChannelModule, MessageModule, AuthModule, ]
+  imports: [ChannelModule, MessageModule, AuthModule, TypeOrmModule.forFeature([ConnectedUserEntity])],
 })
 export class ChatModule {}
