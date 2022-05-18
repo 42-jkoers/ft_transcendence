c6b37a2fb7f7c8361b4269ac4649258ccd4dd03a
Irem Ergun <cikrikciogluirem@gmail.com>
2022-05-18 08:06:37 +0200

    added MessageEntity to keep message info

---
+++ backend/src/chat/message/message.entity.ts
@@ -0,0 +1,33 @@
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	JoinTable,
+	ManyToOne,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../user/user.entity';
+import { RoomEntity } from '../room/entities/room.entity';
+
+@Entity()
+export class MessageEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	text: string;
+
+	@ManyToOne(() => User, (user) => user.messages)
+	user: User;
+
+	@ManyToOne(() => RoomEntity, (room) => room.messages)
+	@JoinTable()
+	room: RoomEntity;
+
+	@CreateDateColumn()
+	created_at: Date;
+
+	@UpdateDateColumn()
+	updated_at: Date;
+}
