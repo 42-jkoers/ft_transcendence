a8f354392c85b1e80e7ec519c843db4be5c75c72
Irem Cikrikcioglu <icikrikc@f1r1s2.codam.nl>
2022-05-18 18:19:58 +0200

    converted class Chat  extends Vue to export default defineComponent - composition API way

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -17,17 +17,14 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+import { defineComponent } from "vue";
 import ChatRooms from "@/components/ChatRooms.vue";
 import ChatBox from "@/components/ChatBox.vue"; // @ is an alias to /src
 
-@Options({
-  components: {
-    ChatRooms,
-    ChatBox,
-  },
-})
-export default class ChatView extends Vue {}
+export default defineComponent({
+  name: "ChatView",
+  components: { ChatRooms, ChatBox },
+});
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped></style>
