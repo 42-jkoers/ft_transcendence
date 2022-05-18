d2f7f4cadc96a42c748743026b731390d51ce54f
Aileen Chen <xchen@student.codam.nl>
2022-05-18 19:23:52 +0200

    set up button to get updated value

---
+++ frontend/src/components/RegisterForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="register-form">
+    <p class="form-title">Register New User</p>
+    <form class="form" @submit.prevent="onSubmit">
+      <div class="input-field">
+        <label class="label">UserName</label>
+        <input
+          class="input-box"
+          type="text"
+          placeholder="User Name"
+          v-model="username"
+        />
+      </div>
+      <br />
+      <div class="input-field">
+        <label class="label">Avatar</label>
+        <input
+          class="input-box"
+          type="text"
+          placeholder="Avatar"
+          v-model="avatar"
+        />
+      </div>
+      <br />
+      <button class="button" @click="sendInput">register</button>
+    </form>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from "vue";
+// reactive state
+const username = ref("");
+const avatar = ref("");
+// functions that mutate state and trigger updates
+function sendInput(args: string) {
+  console.log("username is: ", username.value);
+  console.log("avatar is: ", avatar.value);
+}
+// lifecyle
+// onMounted(() => {});
+</script>
+<style scoped>
+.form-title {
+  color: darkcyan;
+  font-size: large;
+  font-weight: 600;
+}
+.label {
+  padding-right: 12px;
+}
+.input-box::placeholder {
+  color: grey;
+}
+.button {
+  color: green;
+}
+</style>
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,31 +1,19 @@
 <template>
   <div class="register-view">
-    <p class="form-title">Register New User</p>
-    <form class="form">
-      <InputUserName />
-      <br />
-    </form>
-    <button class="button">register</button>
+    <RegisterForm />
+    <br />
   </div>
 </template>
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import InputUserName from "@/components/InputUserName.vue";
+import RegisterForm from "@/components/RegisterForm.vue";
+import { useForm } from "vue-hooks-form";
 
 @Options({
   components: {
-    InputUserName,
+    RegisterForm,
   },
 })
 export default class RegisterView extends Vue {}
 </script>
-<style scoped>
-.form-title {
-  color: darkcyan;
-  font-size: large;
-  font-weight: 600;
-}
-.button {
-  color: green;
-}
-</style>
+<style scoped></style>
