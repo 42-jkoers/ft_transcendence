d41e5ca7b0c14490622c8eb3ed11a864dc544f33
Olga Samara <olcha.olcha@gmail.com>
2022-05-19 18:25:26 +0200

    Routes updated

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import CreateRoom from "@/views/CreateRoom.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -9,16 +10,22 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: "/chat",
-    name: "Chat",
+    name: "chat",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "chat" */ "../views/ChatView.vue"),
+    children: [],
+  },
+  {
+    path: "/chat/create-chatroom",
+    name: "create-chatroom",
+    component: CreateRoom,
   },
   {
     path: "/login",
-    name: "Login",
+    name: "login",
     component: HomeView,
   },
 ];
