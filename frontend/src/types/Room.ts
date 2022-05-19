bb4733115f9079131d9dbc527f41b83a3217a94e
Olga Samara <olcha.olcha@gmail.com>
2022-05-19 18:25:56 +0200

    Room visibility added to frontend

--- frontend/src/types/Room.ts
+++ frontend/src/types/Room.ts
@@ -1,7 +1,9 @@
+import RoomVisibility from "./RoomVisibility";
 interface Room {
   id: null;
   name: string;
   isDirectMessage: boolean;
+  RoomVisibilityType: RoomVisibility;
 }
 
 export default Room;
