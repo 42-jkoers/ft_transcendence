0d136321ebd5d22d979b712c56c6a04ddeb940d3
Olga Samara <olcha.olcha@gmail.com>
2022-05-21 01:01:47 +0200

    Create Room button replaced to ChatView

--- frontend/src/components/ChatRooms.vue
+++ frontend/src/components/ChatRooms.vue
@@ -1,28 +1,13 @@
 <template>
-  <div id="chatrooms">
-    <PrimeVueButton
-      @click="openCreateRoomCard"
-      label="New Chat Room"
-      icon="pi pi-plus"
-      class="p-button-primary"
-    />
-    <router-view></router-view>
-  </div>
+  <div id="chatrooms">List of rooms will be here</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
-import PrimeVueButton from "primevue/button";
 
 export default defineComponent({
   name: "ChatRooms",
-  components: {
-    PrimeVueButton,
-  },
-  methods: {
-    openCreateRoomCard() {
-      this.$router.push({ name: "create-chatroom" });
-    },
-  },
+  components: {},
+  methods: {},
 });
 </script>
