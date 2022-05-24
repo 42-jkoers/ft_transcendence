1f5def853276837dfde6e4e240fca0977d6c71b7
Irem Ergun <cikrikciogluirem@gmail.com>
2022-05-24 15:18:27 +0200

    added Card, InputText and PrimeVueButton to template

--- frontend/src/components/ChatBox.vue
+++ frontend/src/components/ChatBox.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="card">
-    <h1>ChatBox component to display messages</h1>
+  <div id="chatbox">
+    <Card>
+      <template v-slot:content>
+        <div class="card">
+          <div class="flex justify-content-center flex-wrap card-container">
+            <InputText
+              type="text"
+              v-model="input"
+              placeholder="Type your message..."
+              @keyup.enter="sendMessage"
+            >
+            </InputText>
+            <PrimeVueButton
+              @click="sendMessage"
+              icon="pi pi-send"
+              class="p-button-primary"
+            />
+          </div>
+        </div>
+      </template>
+    </Card>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Card from "primevue/card";
+import InputText from "primevue/inputtext";
+import PrimeVueButton from "primevue/button";
+
+//TODO Should input text and button be inside a form tag?
+</script>
