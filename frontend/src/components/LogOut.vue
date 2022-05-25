50defd1f4df839fad6f0b89cc0c0f16d61152f80
Aileen Chen <xchen@student.codam.nl>
2022-05-25 10:28:05 +0200

    change Logout page style

--- frontend/src/components/LogOut.vue
+++ frontend/src/components/LogOut.vue
@@ -1,23 +1,45 @@
 <template>
-  <form class="form" @submit.prevent="onSubmit">
-    <button class="logout-button" @click="requestLogOut">Log out</button>
-    <div v-if="request">
-      <p class="confirm-question">Are you sure you want to log out?</p>
-      <button class="yes-button" @click="confirmLogOut">Confirm</button>
-    </div>
-    <div v-if="confirm">
-      <p class="success-message">You've successfully logged out.</p>
-    </div>
-  </form>
+  <br />
+  <div v-if="logoutButton">
+    <Button
+      label="Log Out"
+      icon="pi pi-power-off"
+      iconPos="left"
+      @click="requestLogOut"
+    />
+  </div>
+  <div v-if="request">
+    <p class="message">Are you sure you want to log out?</p>
+    <span class="p-buttonset">
+      <Button
+        label="Confirm"
+        icon="pi pi-check"
+        iconPos="left"
+        @click="confirmLogOut"
+      />
+      <Button
+        label="Cancel"
+        icon="pi pi-times"
+        iconPos="left"
+        @click="cancelLogOut"
+      />
+    </span>
+  </div>
+  <div v-if="confirm">
+    <p class="message">You've successfully logged out.</p>
+  </div>
 </template>
 <script setup lang="ts">
+import Button from "primevue/button";
 import { ref } from "vue";
 import axios from "axios";
 import store from "@/store/index";
+const logoutButton = ref<boolean>(true);
 const request = ref<boolean>(false);
 const confirm = ref<boolean>(false);
 function requestLogOut() {
   request.value = true;
+  logoutButton.value = false;
 }
 
 async function confirmLogOut() {
@@ -28,14 +50,16 @@ async function confirmLogOut() {
   confirm.value = true;
   store.commit("logout");
 }
-</script>
-<style>
-.confirm-question {
-  color: darkred;
-  font-weight: 500;
+
+function cancelLogOut() {
+  logoutButton.value = true;
+  request.value = false;
 }
-.success-message {
-  color: green;
+</script>
+<style scoped>
+.message {
+  color: white;
+  font-size: x-large;
   font-weight: 500;
 }
 </style>
