ec94aaed4b7769dd8951c069fe4a8c12d286b4e7
Irem Ergun <cikrikciogluirem@gmail.com>
2022-05-28 22:58:03 +0200

    temporary id added to Room interface because querying with name failed

--- frontend/src/types/Room.ts
+++ frontend/src/types/Room.ts
@@ -1,5 +1,6 @@
 import RoomVisibility from "./RoomVisibility";
 interface Room {
+  id: number;
   name: string; // no id but the name must be unique
   isDirectMessage: boolean;
   visibility: RoomVisibility;
