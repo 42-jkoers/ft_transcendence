e7d4879407092ad13830eb5253d8a8839e6eff55
Olga Samara <olcha.olcha@gmail.com>
2022-05-29 23:25:55 +0200

    Non-creator can be added to the existing room

--- backend/src/chat/room/room.service.ts
+++ backend/src/chat/room/room.service.ts
@@ -1,16 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+// import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, getRepository } from 'typeorm';
+// import { RoomEntity, RoomVisibilityType } from './entities/room.entity';
 import { RoomEntity } from './entities/room.entity';
 import { RoomI } from './room.interface';
 import { UserI } from 'src/user/user.interface';
+import { UserService } from '../../user/user.service';
 
 @Injectable()
 export class RoomService {
 	constructor(
+		@Inject(forwardRef(() => UserService))
+		private readonly userService: UserService,
 		@InjectRepository(RoomEntity)
-		private readonly RoomEntityRepository: Repository<RoomEntity>,
+		private readonly RoomEntityRepository: Repository<RoomEntity>, // private readonly UserService: UserService,
 	) {}
+
+	async findByName(roomName: string): Promise<RoomI> {
+		return await this.RoomEntityRepository.findOne({
+			where: { name: roomName },
+		});
+	}
 
 	async createRoom(
 		room: RoomI,
@@ -31,7 +42,44 @@ export class RoomService {
 			response.status = 'OK';
 			response.data = `${newRoom.name}`;
 		} catch (err) {
-			// if promise rejects (in case the name is not unique)
+			// if promise rejects (in case the name is not unique,23505 - is the PostrgreSQL error code for unique constraint violation)
+			if (err.code === '23505') {
+				response.status = 'ERROR';
+				response.data = `${newRoom.name}`;
+			}
+		}
+		return response;
+	}
+
+	//temp:
+	async addUserToRoom(
+		room: RoomI,
+		creator: UserI,
+	): Promise<{ status: string; data: string }> {
+		// const thisroom: RoomI = this.findByName('#general');
+		const userToAdd: UserI = await this.userService.findByID(2);
+		const emptyRoom: RoomI = {
+			name: room.name,
+			visibility: room.visibility, //FIXME: visibiity undefined
+			users: [],
+		};
+		const newRoomFirst = await this.addCreatorToRoom(emptyRoom, creator); // adding current creator to the array of users for this new room
+		const newRoom: RoomI = await this.addCreatorToRoom(
+			newRoomFirst,
+			userToAdd,
+		); // adding current creator to the array of users for this new room
+		console.log('new room: ', newRoom);
+
+		const response = {
+			status: '',
+			data: '',
+		};
+		try {
+			await this.RoomEntityRepository.save(newRoom); // Saves a given entity in the database if the new room name doesn't exist.
+			response.status = 'OK';
+			response.data = `${newRoom.name}`;
+		} catch (err) {
+			// if promise rejects (in case the name is not unique,23505 - is the PostrgreSQL error code for unique constraint violation)
 			if (err.code === '23505') {
 				response.status = 'ERROR';
 				response.data = `${newRoom.name}`;
