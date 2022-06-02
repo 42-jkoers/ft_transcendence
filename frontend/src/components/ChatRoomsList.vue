4abe2f0d6f1f75fce5c560cb9558de7322234534
Olga Samara <olcha.olcha@gmail.com>
2022-06-02 15:16:26 +0200

    Icons in the rooms list reflect rooms visibility

--- frontend/src/components/ChatRoomsList.vue
+++ frontend/src/components/ChatRoomsList.vue
@@ -2,6 +2,7 @@
   <div id="chatrooms-list">
     <DataTable
       :value="rooms"
+      class="p-datatable-sm"
       responsiveLayout="scroll"
       :scrollable="true"
       scrollHeight="60vh"
@@ -11,15 +12,38 @@
       dataKey="name"
       @rowSelect="onRowSelect"
     >
-      <Column field="name" header="Chat Rooms"></Column>
+      <Column field="visibility" style="max-width: 2.5rem">
+        <template #body="slotProps">
+          <div>
+            <i
+              v-if="slotProps.data.visibility === RoomVisibility.PUBLIC"
+              class="pi pi-hashtag"
+              style="font-size: 0.8rem"
+            ></i>
+            <i
+              v-else-if="slotProps.data.visibility === RoomVisibility.PRIVATE"
+              class="pi pi-lock"
+              style="font-size: 0.8rem"
+            ></i>
+            <i v-else class="pi pi-shield" style="font-size: 0.8rem"></i>
+          </div>
+        </template>
+      </Column>
+      <Column
+        field="name"
+        bodyStyle="padding:0"
+        header="Chat Rooms"
+        headerStyle="padding-left:0"
+      ></Column>
     </DataTable>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, inject, onMounted } from "vue";
-import { Socket } from "socket.io-client";
 import { useRouter, useRoute } from "vue-router";
+import { Socket } from "socket.io-client";
+import RoomVisibility from "@/types/RoomVisibility";
 
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
