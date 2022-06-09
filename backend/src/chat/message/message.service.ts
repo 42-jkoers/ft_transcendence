0102ecbbe3c027ef2f58c0edea906f7685799a58
Irem Ergun <cikrikciogluirem@gmail.com>
2022-06-09 22:15:09 +0200

    passed room and user from gateway's addMessage function and added to interface before creating the message in repository

--- backend/src/chat/message/message.service.ts
+++ backend/src/chat/message/message.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserI } from 'src/user/user.interface';
 import { Repository } from 'typeorm';
+import { RoomI } from '../room/room.interface';
 import { MessageEntity } from './message.entity';
 import { MessageI } from './message.interface';
 
@@ -11,7 +13,13 @@ export class MessageService {
 		private readonly messageRepository: Repository<MessageEntity>,
 	) {}
 
-	async create(message: MessageI): Promise<MessageI> {
+	async create(
+		message: MessageI,
+		user: UserI,
+		room: RoomI,
+	): Promise<MessageI> {
+		message.user = user;
+		message.room = room; //TODO ask if modifying interface is best practice
 		return this.messageRepository.save(
 			this.messageRepository.create(message),
 		);
