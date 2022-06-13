34da718c65047bd67ddac9847f7c49a4e326d990
Aileen Chen <xchen@student.codam.nl>
2022-06-13 19:31:32 +0200

    load request list as DataTable in FriendsView

--- frontend/src/components/FriendRequest.vue
+++ frontend/src/components/FriendRequest.vue
@@ -1,16 +1,51 @@
 <template>
-  <div>{{ requestList }}</div>
+  <div>
+    <DataTable :value="requests" responsiveLayout="scroll">
+      <Column
+        field="username"
+        header="Requested User"
+        headerStyle="width: 30%"
+      ></Column>
+      <Column header="Action" headerStyle="width: 70%">
+        <template #body>
+          <div>
+            <span class="p-buttonset">
+              <Button
+                label="Approve"
+                class="p-button-success"
+                icon="pi pi-check"
+                iconPos="left"
+                style="width: 30%"
+              />
+              <Button
+                label="Reject"
+                class="p-button-danger"
+                icon="pi pi-times"
+                iconPos="left"
+                style="width: 30%"
+              />
+            </span>
+          </div>
+        </template>
+      </Column>
+    </DataTable>
+  </div>
 </template>
 <script setup lang="ts">
 import { onMounted, ref, inject } from "vue";
 import { Socket } from "socket.io-client";
+import Button from "primevue/button";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 
 const socket: Socket = inject("socketioInstance");
-const requestList = ref();
+const requests = ref();
 onMounted(() => {
-  socket.emit("getFriendRequests");
+  setTimeout(() => {
+    socket.emit("getFriendRequests");
+  }, 90); // FIXME: find a better solution?
   socket.on("getFriendRequests", (response) => {
-    requestList.value = response;
+    requests.value = response;
   });
 });
 </script>
--- frontend/src/views/FriendsView.vue
+++ frontend/src/views/FriendsView.vue
@@ -1,5 +1,7 @@
 <template>
-  <FriendRequest />
+  <div class="col-offset-2 col-8" align="center">
+    <FriendRequest />
+  </div>
 </template>
 <script setup lang="ts">
 import FriendRequest from "@/components/FriendRequest.vue";
