60277f3cfec4e361379ad94ec0bc9c5e3c64b083
Aileen Chen <xchen@student.codam.nl>
2022-06-16 13:25:49 +0200

    add confirmation dialog to EditFriendButton

--- frontend/src/components/EditFriendButton.vue
+++ frontend/src/components/EditFriendButton.vue
@@ -1,9 +1,11 @@
 <template>
+  <Toast />
+  <ConfirmDialog></ConfirmDialog>
   <Button
     class="p-button-rounded p-button-text p-button-outlined"
     :label="props.buttonLabel"
     :icon="props.buttonIcon"
-    @click="editFriend(props.friendId, props.action)"
+    @click="confirm1"
   />
 </template>
 <script setup lang="ts">
@@ -11,13 +13,42 @@ import { defineEmits, defineProps } from "vue";
 import Button from "primevue/button";
 import axios from "axios";
 import storeUser from "@/store";
-
+import { useConfirm } from "primevue/useconfirm";
+import { useToast } from "primevue/usetoast";
+import ConfirmDialog from "primevue/confirmdialog";
+import Toast from "primevue/toast";
 const props = defineProps({
   friendId: Number,
   buttonLabel: String,
   buttonIcon: String,
   action: Number,
 });
+const confirm = useConfirm();
+const toast = useToast();
+function confirm1() {
+  confirm.require({
+    message: "Are you sure you want to proceed?",
+    header: "Confirmation",
+    icon: "pi pi-exclamation-triangle",
+    accept: () => {
+      toast.add({
+        severity: "info",
+        summary: "Confirmed",
+        detail: "You have confirmed",
+        life: 3000,
+      });
+      editFriend(props.friendId, props.action);
+    },
+    reject: () => {
+      toast.add({
+        severity: "error",
+        summary: "Rejected",
+        detail: "You have cancelled",
+        life: 3000,
+      });
+    },
+  });
+}
 
 const emit = defineEmits<{
   (event: "isActionSuccess"): boolean;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,9 +7,13 @@ import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
 import "primeflex/primeflex.css"; //PrimeFlex
 import storeUser from "@/store";
+import ConfirmationService from "primevue/confirmationservice"; //Primevue confirmation dialog
+import ToastService from "primevue/toastservice"; // Primevue notification message
 
 const app = createApp(App);
 app.use(storeUser);
 app.use(router);
 app.use(PrimeVue);
+app.use(ConfirmationService);
+app.use(ToastService);
 app.mount("#app");
