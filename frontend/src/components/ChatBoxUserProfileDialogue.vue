79e39ffe65485a090e7dd3160761bf41601bb889
ergunirem <cikrikciogluirem@gmail.com>
2022-06-17 12:16:19 +0200

    View Profile button's pushToProfile function routes to UserProfileCard

--- frontend/src/components/ChatBoxUserProfileDialogue.vue
+++ frontend/src/components/ChatBoxUserProfileDialogue.vue
@@ -1,10 +1,10 @@
 <template>
   <Dialog
-    header="User Profile"
+    :header="clickedUserObject.username"
     :visible="isDialogVisible"
     closeOnEscape
-    :closable="false"
-    position="right"
+    :closable="true"
+    position="center"
   >
     <div class="field p-fluid">
       <Image
@@ -15,24 +15,15 @@
       />
     </div>
     <template #footer>
-      <Button
-        label="Message"
-        icon="pi pi-envelope"
-        @click="sendDM"
-        class="p-button-text"
-      />
-      <Button
-        label="View Profile"
-        icon="pi pi-user"
-        @click="pushToProfile"
-        autofocus
-      />
+      <Button label="Message" icon="pi pi-envelope" @click="sendDM" />
+      <Button label="View Profile" icon="pi pi-user" @click="pushToProfile" />
     </template>
   </Dialog>
 </template>
 
 <script setup lang="ts">
 import { ref, defineEmits, defineProps } from "vue";
+import { useRouter } from "vue-router";
 import Dialog from "primevue/dialog";
 import Button from "primevue/button";
 import Image from "primevue/image"; //TODO style img width
@@ -41,4 +32,12 @@ const props = defineProps(["isDialogVisible", "clickedUserObject"]);
 console.log(props.clickedUserObject);
 
 const emit = defineEmits(["update:isDialogVisible"]);
+
+const router = useRouter();
+const pushToProfile = () => {
+  router.push({
+    name: "UserProfileCard",
+    params: { id: props.clickedUserObject.id },
+  });
+};
 </script>
