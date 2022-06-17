6b20c32e1ce04a03122c457852bd3ce10f7c2460
ergunirem <cikrikciogluirem@gmail.com>
2022-06-17 08:21:12 +0200

    Dialog from PrimeVue added & emit and props deinfed

---
+++ frontend/src/components/ChatBoxUserProfileDialogue.vue
@@ -0,0 +1,18 @@
+<template>
+  <Dialog
+    header="User Profile"
+    :visible="isDialogVisible"
+    closeOnEscape
+    :closable="false"
+  >
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, defineEmits, defineProps } from "vue";
+import Dialog from "primevue/dialog";
+
+const props = defineProps(["isDialogVisible", "clickedUserObject"]);
+
+const emit = defineEmits(["update:isDialogVisible"]);
+</script>
