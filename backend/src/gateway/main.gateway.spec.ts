9aec67bcbba1863db00fbcc6e32733a85d0bcfa4
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2022-06-20 12:22:51 +0200

    rename files

--- backend/src/gateway/main.gateway.spec.ts
+++ backend/src/gateway/main.gateway.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ChatGateway } from './chat.gateway';
+import { ChatGateway } from './main.gateway';
 
 describe('ChatGateway', () => {
 	let gateway: ChatGateway;
