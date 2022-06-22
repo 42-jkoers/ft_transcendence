b3e39d3a880953a8f4f481075e3d19e767dbe5b8
ergunirem <cikrikciogluirem@gmail.com>
2022-06-22 16:55:52 +0200

    userList defined and DataTable added

--- frontend/src/components/ChatBoxAddUsersDialogue.vue
+++ frontend/src/components/ChatBoxAddUsersDialogue.vue
@@ -7,15 +7,18 @@
     <template #header>
       <h3>Users</h3>
     </template>
+    <DataTable :value="userList"> </DataTable>
   </Dialog>
 </template>
 
 <script setup lang="ts">
 import { ref, defineEmits, defineProps } from "vue";
 import Dialog from "primevue/dialog";
+import DataTable from "primevue/datatable";
 
 const props = defineProps(["isDialogVisible"]);
 const emit = defineEmits(["update:isDialogVisible"]);
+const userList = ref([]);
 
 const handleClose = () => {
   emit("update:isDialogVisible", false);
