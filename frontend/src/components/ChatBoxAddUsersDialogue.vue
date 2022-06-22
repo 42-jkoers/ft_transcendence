fa46d99558ca7b8211cafd2abbd5fb6050052efa
ergunirem <cikrikciogluirem@gmail.com>
2022-06-22 16:31:24 +0200

    ChatBoxAddUsersDialogue created with a simple dialog with a header

---
+++ frontend/src/components/ChatBoxAddUsersDialogue.vue
@@ -0,0 +1,23 @@
+<template>
+  <Dialog
+    :visible="isDialogVisible"
+    :style="{ width: '50vw' }"
+    @update:visible="handleClose"
+  >
+    <template #header>
+      <h3>Users</h3>
+    </template>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, defineEmits, defineProps } from "vue";
+import Dialog from "primevue/dialog";
+
+const props = defineProps(["isDialogVisible"]);
+const emit = defineEmits(["update:isDialogVisible"]);
+
+const handleClose = () => {
+  emit("update:isDialogVisible", false);
+};
+</script>
