94374ce6bb1b90515200c797276c5ec271d37a0f
ergunirem <cikrikciogluirem@gmail.com>
2022-06-22 17:03:36 +0200

    socket injected & getNonMemberUsers socket event registered & response saved to userList

--- frontend/src/components/ChatBoxAddUsersDialogue.vue
+++ frontend/src/components/ChatBoxAddUsersDialogue.vue
@@ -12,13 +12,20 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits, defineProps } from "vue";
+import { ref, defineEmits, defineProps, inject } from "vue";
+import { Socket } from "socket.io-client";
 import Dialog from "primevue/dialog";
 import DataTable from "primevue/datatable";
 
+const socket: Socket = inject("socketioInstance");
 const props = defineProps(["isDialogVisible"]);
 const emit = defineEmits(["update:isDialogVisible"]);
-const userList = ref([]);
+const userList = ref();
+
+socket.emit("getNonMemberUsers");
+socket.on("getNonMemberUsers", (response) => {
+  userList.value = response;
+});
 
 const handleClose = () => {
   emit("update:isDialogVisible", false);
