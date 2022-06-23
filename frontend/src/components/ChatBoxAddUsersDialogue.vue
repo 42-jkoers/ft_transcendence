e9615c1b40d9b77732e5dcb0b669e469d3ebb657
ergunirem <cikrikciogluirem@gmail.com>
2022-06-23 20:55:43 +0200

    added rowClick to DataTable and upon click it emits to backend with username and roomname to be added

--- frontend/src/components/ChatBoxAddUsersDialogue.vue
+++ frontend/src/components/ChatBoxAddUsersDialogue.vue
@@ -11,6 +11,7 @@
       scrollHeight="60vh"
       :row-hover="true"
       dataKey="id"
+      @rowClick="onRow"
     >
       <Column field="username" header="Users"> </Column>
     </DataTable>
@@ -20,19 +21,31 @@
 <script setup lang="ts">
 import { ref, defineEmits, defineProps, inject } from "vue";
 import { Socket } from "socket.io-client";
+import { useRoute } from "vue-router";
 import Dialog from "primevue/dialog";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 
 const socket: Socket = inject("socketioInstance");
+const route = useRoute();
 const props = defineProps(["isDialogVisible"]);
 const emit = defineEmits(["update:isDialogVisible"]);
 const userList = ref();
 
-socket.emit("getNonMemberUsers");
+socket.emit("getNonMemberUsers", "general");
+
 socket.on("getNonMemberUsers", (response) => {
   userList.value = response;
 });
+
+const onRow = (event) => {
+  const user = event.data;
+  console.log(`room ${route.params.roomName} user ${user.username} added?`);
+  socket.emit("userBeingAddedToRoom", {
+    userName: user.username,
+    roomName: route.params.roomName,
+  });
+};
 
 const handleClose = () => {
   emit("update:isDialogVisible", false);
