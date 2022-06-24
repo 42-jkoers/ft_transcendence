c09cb04f62e9a5e25a2f2d757e033113dea254b8
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2022-06-24 17:52:26 +0200

    show invalid input message on frontend

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,12 +1,20 @@
 <template>
   <p class="message">This is the game page</p>
   <form class="form" @submit.prevent="onSubmit">
-    <input
-      v-model="input"
-      placeholder="Create a game..."
-      class="input"
-      @keyup.enter="createGame"
-    />
+    <span class="p-float-label">
+      <InputText
+        id="game-name"
+        type="text"
+        class="game-name-input"
+        v-model="newGameName"
+        required="true"
+        maxlength="64"
+        @keyup.enter="createGame"
+      />
+      <small v-if="!isValidGameName" id="game-name" class="p-error">{{
+        invalidGameNameResponseMessage
+      }}</small>
+    </span>
     <div class="input-group-append">
       <button class="send-button" @click="createGame">Create game</button>
     </div>
@@ -14,17 +22,27 @@
 </template>
 
 <script setup lang="ts">
-import { inject, onMounted } from "vue";
+import { inject, onMounted, ref } from "vue";
 import { Socket } from "socket.io-client";
+import InputText from "primevue/inputtext";
 
 const socket: Socket = inject("socketioInstance");
 // const games: ref<Array<GameI>>([]);
 
+const newGameName = ref<string>("");
+const isValidGameName = ref<boolean>(true);
+const invalidGameNameResponseMessage = ref<string>("");
+socket.on("BadRequestException", (response) => {
+  isValidGameName.value = false;
+  invalidGameNameResponseMessage.value = response.message[0];
+});
+
 onMounted(() => {
   console.log("mounted");
 });
+
 function createGame() {
-  console.log("createGame");
-  socket.emit("createGame", { name: `game at ${new Date().toISOString()}` });
+  console.log("createGame", newGameName.value);
+  socket.emit("createGame", { name: newGameName.value });
 }
 </script>
