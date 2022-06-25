e2eb48a49c9069d739835a09d8c89554ebb34317
ergunirem <cikrikciogluirem@gmail.com>
2022-06-25 10:34:25 +0300

    add user button fetched all users but a toast for error for member users & only nonmembers are added to the room with gateway socket emituserAddsAnotherUserToRoom

--- frontend/src/components/ChatBoxAddUsersDialogue.vue
+++ frontend/src/components/ChatBoxAddUsersDialogue.vue
@@ -19,32 +19,56 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits, defineProps, inject } from "vue";
+import { ref, defineEmits, defineProps, inject, computed } from "vue";
 import { Socket } from "socket.io-client";
 import { useRoute } from "vue-router";
 import Dialog from "primevue/dialog";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
+import { useToast } from "primevue/usetoast";
 
 const socket: Socket = inject("socketioInstance");
 const route = useRoute();
 const props = defineProps(["isDialogVisible"]);
 const emit = defineEmits(["update:isDialogVisible"]);
+const toast = useToast();
 const userList = ref();
+const roomWithUserRole = ref();
 
-socket.emit("getNonMemberUsers", route.params.roomName);
-
-socket.on("getNonMemberUsers", (response) => {
+socket.emit(
+  "getAllRegisteredUsersExceptYourselfAndAdmin",
+  route.params.roomName
+);
+socket.on("getAllRegisteredUsersExceptYourselfAndAdmin", (response) => {
   userList.value = response;
+});
+
+socket.emit("getOneRoomWithUserToRoomRelations", route.params.roomName);
+socket.on("getOneRoomWithUserToRoomRelations", (response) => {
+  roomWithUserRole.value = response;
 });
 
 const onRow = (event) => {
   const user = event.data;
-  console.log(`room ${route.params.roomName} user ${user.username} added?`);
-  socket.emit("userAddsAnotherUserToRoom", {
-    userId: user.id,
-    roomName: route.params.roomName,
-  });
+  if (checkIfUserIsInRoom(user.username)) {
+    toast.add({
+      severity: "error",
+      summary: "Error",
+      detail: "User is already in the room",
+      life: 1000,
+    });
+  } else {
+    console.log(`room ${route.params.roomName} user ${user.username} added`);
+    socket.emit("userAddsAnotherUserToRoom", {
+      userId: user.id,
+      roomName: route.params.roomName,
+    });
+  }
+};
+
+const checkIfUserIsInRoom = (userName: string) => {
+  for (var userToRoom of roomWithUserRole.value.userToRooms)
+    if (userToRoom.user.username === userName) return true;
 };
 
 const handleClose = () => {
