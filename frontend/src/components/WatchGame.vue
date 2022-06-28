e55da38eb7d02fd3f8900728c78543088ddb2b29
Aileen Chen <aileenchen14@gmail.com>
2022-06-28 17:41:56 +0200

    add game controller

--- frontend/src/components/WatchGame.vue
+++ frontend/src/components/WatchGame.vue
@@ -1,2 +1,14 @@
 <template>This is Watch Game</template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from "axios";
+import { onMounted } from "vue";
+
+onMounted(async () => {
+  await axios
+    .get("http://localhost:3000/game/list", { withCredentials: true })
+    .then((response) => {
+      console.log("game list: ", response.data);
+    })
+    .catch();
+});
+</script>
