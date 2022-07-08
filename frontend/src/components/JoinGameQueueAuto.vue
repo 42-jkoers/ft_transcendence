d4091675d131a404d6938149398339685818409a
Aileen Chen <aileenchen14@gmail.com>
2022-07-08 10:20:57 +0200

    delete not-used socket

--- frontend/src/components/JoinGameQueueAuto.vue
+++ frontend/src/components/JoinGameQueueAuto.vue
@@ -10,13 +10,9 @@
 </template>
 <script setup lang="ts">
 import Button from "primevue/button";
-import { Socket } from "socket.io-client";
-import { inject } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-
-const socket: Socket = inject("socketioInstance") as Socket;
 
 function joinWaitingRoom() {
   router.push({ name: "GameWaitingRoom", params: { type: "auto" } });
