061ed44683bb48b01d0f1ff919d952ea0db1007e
Olga Samara <olcha.olcha@gmail.com>
2022-07-11 15:25:12 +0200

    Small introduction added to the user home page

--- frontend/src/views/UserHomeView.vue
+++ frontend/src/views/UserHomeView.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
-    <h2>This is the user's home page, to be added.</h2>
+    <h2>Welcome to our pong place. Meet other people and play pong together</h2>
+  </div>
+  <div class="card">
+    <div class="flex justify-content-center flex-wrap card-container mt-4">
+      <Button
+        @click="goToChat"
+        label="Chat with people"
+        class="flex align-items-center justify-content-center border-round m-4"
+      />
+      <Button
+        @click="goToGame"
+        label="Play Pong"
+        class="flex align-items-center justify-content-center border-round m-4"
+      />
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+import Button from "primevue/button";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const goToChat = () => {
+  router.push({
+    name: "Chat",
+  });
+};
+const goToGame = () => {
+  router.push({
+    name: "Game",
+  });
+};
+</script>
+<style>
+h2 {
+  color: rgb(203, 206, 208);
+}
+</style>
