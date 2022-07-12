5295de58de1602965c7dc6d2d85bb03143edaaaa
Olga Samara <olcha.olcha@gmail.com>
2022-07-12 14:17:08 +0200

    Game mode options toggle button added

--- frontend/src/components/GameCustomizationOptionsDialog.vue
+++ frontend/src/components/GameCustomizationOptionsDialog.vue
@@ -1,21 +1,13 @@
 <template>
   <Dialog
-    header="Customize your game"
+    header="Choose your game mode"
     :visible="isDialogVisible"
-    :style="{ width: '50vw' }"
     @update:visible="handleClose"
   >
-    <div class="card flex align-items-center justify-content-start">
-      <h5>Select paddle color</h5>
-      <ColorPicker v-model="paddleColor" class="m-4" />
+    <div class="card flex align-items-center justify-content-center">
+      <SelectButton v-model="gameSpeedOption" :options="options" class="" />
     </div>
     <template #footer>
-      <Button
-        label="Reset"
-        icon="pi pi-refresh"
-        class="p-button-text"
-        @click="reset"
-      />
       <Button
         label="Save"
         icon="pi pi-check"
@@ -30,32 +22,23 @@ import { defineEmits, defineProps, inject, ref } from "vue";
 import { Socket } from "socket.io-client";
 import Dialog from "primevue/dialog";
 import Button from "primevue/button";
-import ColorPicker from "primevue/colorpicker";
+
+import SelectButton from "primevue/selectbutton";
 
 defineProps(["isDialogVisible"]);
 const emit = defineEmits(["update:isDialogVisible"]);
 
 const socket: Socket = inject("socketioInstance") as Socket;
-const defaultPaddleValues = {
-  paddleColor: "fffafa",
-  // paddleSize: "1",
-};
-const paddleColor = ref("fffafa");
-// const paddleSize = ref("1");
 
-const reset = () => {
-  paddleColor.value = defaultPaddleValues.paddleColor;
-  // paddleSize.value = defaultPaddleValues.paddleSize;
-};
+const gameSpeedOption = ref<string>("Slow");
+const options = ref(["Slow", "Fast"]);
 
 const saveNewGameSettings = () => {
-  // console.log("paddleColor value", paddleColor.value);
-  socket.emit("saveUserCustomizationOptions", { paddleColor: paddleColor });
+  socket.emit("saveUserCustomizationOptions", { gameSpeed: gameSpeedOption });
   emit("update:isDialogVisible", false);
 };
 
 const handleClose = () => {
-  reset();
   emit("update:isDialogVisible", false);
 };
 </script>
