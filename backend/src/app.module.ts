5992fb12ee378977731cbfc47577a4e353996fb4
Aileen Chen <aileenchen14@gmail.com>
2022-07-13 17:24:24 +0200

    remove not needed content

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,4 @@
-import {
-	MiddlewareConsumer,
-	Module,
-	NestModule,
-	RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,7 +7,6 @@ import { ChatModule } from './chat/chat.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { PassportModule } from '@nestjs/passport';
-import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
 	imports: [
@@ -26,12 +20,4 @@ import { LoggerMiddleware } from './logger.middleware';
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {
-	// TODO remove later, this is to implement the logger for the middleware
-	// export class AppModule implements NestModule{
-	// 	configure(consumer: MiddlewareConsumer) {
-	// 		consumer
-	// 		.apply(LoggerMiddleware)
-	// 		.forRoutes({ path: 'auth/2f', method: RequestMethod.POST });
-	// 	}
-}
+export class AppModule {}
