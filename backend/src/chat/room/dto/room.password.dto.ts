ff6fe63d9cb6db81704c12d6a1e7e75cb105657b
Olga Samara <olcha.olcha@gmail.com>
2022-07-13 15:22:46 +0200

    conditional  password validation for edit privacy bugfix

--- backend/src/chat/room/dto/room.password.dto.ts
+++ backend/src/chat/room/dto/room.password.dto.ts
@@ -1,11 +1,18 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, ValidateIf } from 'class-validator';
 
 export class RoomPasswordDto {
 	@IsString()
 	@IsNotEmpty()
 	roomName: string;
 
+	@ValidateIf((obj) => {
+		return obj && obj.password !== null;
+	})
 	@IsString()
-	@IsNotEmpty()
+	@MaxLength(64, {
+		message: () => {
+			return 'This password is too long. Please create a shorter one';
+		},
+	}) // Maximum password length should not be set too low, as it will prevent users from creating passphrases
 	password: string;
 }
